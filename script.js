008b4b4fd212d4b3dd38022da92e03ae039e0060
Tay-juni <124503529+Tay-juni@users.noreply.github.com>
2023-11-06 09:01:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let BtnBoutton = document.getElementById("BtnBoutton");
+let BtnCappu  = document.getElementById("BtnCappu");
+let BtnThe  = document.getElementById("BtnThe");
+let BtnPetit  = document.getElementById("BtnPetit");
